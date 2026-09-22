ce3f4e6f6475192498017eb6b3190a2ed81a23c0
362573354 <362573354@2260I-L29-007>
2026-09-22 11:47:46 -0400

    Add project files.

---
+++ variables assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace variables_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string room, price, learning, name, gradyear, circleradius, circlearea, rectanglelength, rectanglewidth, rectanglearea;
+            const double pi = 3.14;
+            room = "29";
+            price = "$2.99";
+            learning = "Computer Science";
+            gradyear = "2029";
+            circleradius = "6";
+            circlearea = (pi * Math.Pow(double.Parse(circleradius), 2)).ToString();
+            name = "Damien Klassen";
+            rectanglelength = "19";
+            rectanglewidth = "7";
+            rectanglearea = (double.Parse(rectanglelength) * double.Parse(rectanglewidth)).ToString();
+            Console.WriteLine("this is room " + room);
+            Console.WriteLine("The price is " + price);
+            Console.WriteLine("I am learning a bit about " + learning);
+            Console.WriteLine("My name is " + name + " and I'll graduate in " + gradyear);
+            Console.WriteLine("The area of a circle with a radius of " + circleradius + " is " + circlearea);
+            Console.WriteLine("The area of a rectangle with a length of " + rectanglelength + " and a width of " + rectanglewidth + " is " + rectanglearea);
+
+        }
+    }
+}
